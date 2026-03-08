6e1b742ade221d28cced274134d42d8d8160ec09
gabrielberlucci <gabrielberluccidev@proton.me>
2026-03-08 13:42:58 -0300

    feat: add variable 3000 port on api start

--- server/src/index.ts
+++ server/src/index.ts
@@ -52,6 +52,6 @@ app.get('/:shortUrl', async (req: Request, res: Response) => {
   }
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
