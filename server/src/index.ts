ae2bea1c1fa64ea01999e6ea8e4325209737f7e6
gabrielberlucci <gabrielberluccidev@proton.me>
2026-03-08 22:37:28 -0300

    feat: add ZOD validation

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,9 @@ import 'dotenv/config';
 import { prisma } from '@lib/prisma.js';
 import cors from 'cors';
 import { nanoid } from 'nanoid';
+import z, { ZodError } from 'zod';
+
+import { urlSchema } from '@shared/index.js';
 
 const app = express();
 app.use(cors());
@@ -12,6 +15,7 @@ app.post('/api/url', async (req: Request, res: Response) => {
   try {
     const originalUrl = req.body['longUrl'] as string;
 
+    urlSchema.parse(originalUrl);
     const shortUrl = `${process.env.BASE_URL}/${nanoid(8)}`;
 
     const newUrl = await prisma.url.create({
@@ -28,6 +32,12 @@ app.post('/api/url', async (req: Request, res: Response) => {
       },
     });
   } catch (error) {
+    if (error instanceof ZodError) {
+      const flattened = z.flattenError(error);
+      res.status(400).send({
+        error: flattened.formErrors[0],
+      });
+    }
     res.status(500).send({
       message: error,
     });
@@ -45,6 +55,13 @@ app.get('/:shortUrl', async (req: Request, res: Response) => {
         shortUrl: finalUrl,
       },
     });
+
+    if (!urlData) {
+      res.status(404).send({
+        message: `Short URL ${shortUrl} not founded`,
+      });
+      return;
+    }
 
     res.redirect(urlData?.longUrl!);
   } catch (error) {
